b26a33a0af5398adabeae41bf31a62d75ac2933a
Rajat Jindal <rajatjindal83@gmail.com>
2024-08-13 06:47:20 +0530

    use different implementation for strcase
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- dagql/server.go
+++ dagql/server.go
@@ -11,7 +11,7 @@ import (
 	"sync"
 
 	"github.com/99designs/gqlgen/graphql"
-	"github.com/iancoleman/strcase"
+	"github.com/dagger/dagger/engine/strcase"
 	"github.com/opencontainers/go-digest"
 	"github.com/sourcegraph/conc/pool"
 	"github.com/vektah/gqlparser/v2/ast"
@@ -916,7 +916,7 @@ func setInputObjectFields(obj any, vals map[string]any) error {
 		fieldV := objV.Elem().Field(i)
 		name := fieldT.Tag.Get("name")
 		if name == "" {
-			name = strcase.ToLowerCamel(fieldT.Name)
+			name = strcase.ToCamel(fieldT.Name)
 		}
 		if name == "-" {
 			continue
@@ -981,7 +981,7 @@ func collectLiteralArgs(obj any) ([]*call.Argument, error) {
 		fieldT := objT.Field(i)
 		name := fieldT.Tag.Get("name")
 		if name == "" {
-			name = strcase.ToLowerCamel(fieldT.Name)
+			name = strcase.ToCamel(fieldT.Name)
 		}
 		if name == "-" {
 			continue
